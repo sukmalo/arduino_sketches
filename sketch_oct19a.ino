0519ccf1f32066435995e030420005bcc691d500
sukmalo <114556628+sukmalo@users.noreply.github.com>
2022-10-27 18:54:23 +0800

    Пункт 4

--- sketch_oct19a.ino
+++ sketch_oct19a.ino
@@ -1,141 +1,38 @@
-#define A  8
-#define B  7
-#define C  6
-#define D  5
-#define E  4
-#define F  3
-#define G  2
-#define sensor_pin A2
-int count = 0;
-
+int buttonPin = 9;
+int ledPin = 3;
+bool light = 0;
+int count = 10;
+int step = 10;
 void setup() {
-
   Serial.begin(9600);
-
-   pinMode(A, OUTPUT);
-   pinMode(B, OUTPUT);
-   pinMode(C, OUTPUT);
-   pinMode(D, OUTPUT);
-   pinMode(E, OUTPUT);
-   pinMode(F, OUTPUT);
-   pinMode(G, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  int val = analogRead(sensor_pin);
+  bool val = digitalRead(buttonPin);
   Serial.println(val);
-
-  if (val>400){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, HIGH);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, LOW);
-    count = count + 1;
-    if (count % 10 == 0){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, HIGH);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, LOW);
-    delay(3000);
-  } else if(count % 10 == 1){
-    digitalWrite(A, LOW); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, LOW);
-    digitalWrite(E, LOW);
-    digitalWrite(F, LOW);
-    digitalWrite(G, LOW);
-    delay(3000);
-  }else if(count % 10 == 2){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, LOW);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, HIGH);
-    digitalWrite(F, LOW);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  } else if(count % 10 == 3){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, LOW);
-    digitalWrite(F, LOW);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }else if(count % 10 == 4){
-    digitalWrite(A, LOW); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, LOW);
-    digitalWrite(E, LOW);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }else if(count % 10 == 5){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, LOW);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, LOW);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }else if(count % 10 == 6){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, LOW);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, HIGH);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }else if(count % 10 == 7){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, LOW);
-    digitalWrite(E, LOW);
-    digitalWrite(F, LOW);
-    digitalWrite(G, LOW);
-    delay(3000);
-  }
-  else if(count % 10 == 8){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, HIGH);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }
-  else if(count % 10 == 9){
-    digitalWrite(A, HIGH); 
-    digitalWrite(B, HIGH);
-    digitalWrite(C, HIGH);
-    digitalWrite(D, HIGH);
-    digitalWrite(E, LOW);
-    digitalWrite(F, HIGH);
-    digitalWrite(G, HIGH);
-    delay(3000);
-  }
+  Serial.println(digitalRead(buttonPin));
+  if (val!=digitalRead(buttonPin) && val==1){
+  light = !light; 
+  digitalWrite(ledPin, light);   
+  delay(100);   
   }
   else{
-    digitalWrite(A, LOW); 
-    digitalWrite(B, LOW);
-    digitalWrite(C, LOW);
-    digitalWrite(D, LOW);
-    digitalWrite(E, LOW);
-    digitalWrite(F, LOW);
-    digitalWrite(G, LOW);
-    count = 0;
+    digitalWrite(ledPin, light);
   }
+  if(val==digitalRead(buttonPin) && val==1 && light==1){     
+      analogWrite(ledPin, count);
+      delay(30);   
+      count = count + step;
+      Serial.println(count);
+      if (count == 10 || count == 350) {
+        step = -step ;
+      }
+  if(val==digitalRead(buttonPin) && val==0){     
+      light = !light; 
+      digitalWrite(ledPin, light);   
+      delay(100);  
+  }
+  } 
 }
